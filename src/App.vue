cb728d0f76d4e74c9c001a5d32a5be2008f05810
denise <150321513+denisVlaso@users.noreply.github.com>
2023-12-06 13:09:40 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class=dialog>
+    <People></People>
+    <Message></Message></div>
+    <div>
+        <Footer />
+    </div>
+
+    
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Message from './components/Message.vue';
+import People from './components/People.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Message,
+    People,
+    Footer
+  }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    padding:0;
+    background-color: rgb(108, 168, 221);
+    display: block;
+}
+.dialog{
+    width:154.4vh;
+    display: flex;
+}
+</style>
